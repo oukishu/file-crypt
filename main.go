61384dfe95a018d058c781af5b5f4ec2dba1a731
oukishu <89119588+oukishu@users.noreply.github.com>
2026-05-04 17:56:02 +0800

    Update main.go

--- main.go
+++ main.go
@@ -28,32 +28,49 @@ func deriveKey(password string, salt []byte) []byte {
 	return pbkdf2.Key([]byte(password), salt, iterations, 32, sha256.New)
 }
 
-func encryptFile(src, dst, password string) error {
-	fIn, err := os.Open(src)
-	if err != nil { return err }
-	defer fIn.Close()
+func deriveKeyWeb(password string) []byte {
+	if password == "" {
+		password = "rock-solid-default"
+	}
+	hash := sha256.Sum256([]byte(password))
+	return hash[:]
+}
 
-	fOut, err := os.Create(dst)
+func encryptFile(src, dst, password string, compatMode bool) error {
+	data, err := os.ReadFile(src)
 	if err != nil { return err }
-	defer fOut.Close()
 
+	if compatMode {
+		key := deriveKeyWeb(password)
+		block, _ := aes.NewCipher(key)
+		aesgcm, _ := cipher.NewGCM(block)
+		
+		iv := make([]byte, 12)
+		io.ReadFull(rand.Reader, iv)
+		sealed := aesgcm.Seal(iv, iv, data, nil)
+		return os.WriteFile(dst, sealed, 0644)
+	}
+
+	fIn, _ := os.Open(src); defer fIn.Close()
+	fOut, _ := os.Create(dst); defer fOut.Close()
+	
 	salt := make([]byte, saltSize)
 	masterNonce := make([]byte, noncePrefixSize)
 	io.ReadFull(rand.Reader, salt)
 	io.ReadFull(rand.Reader, masterNonce)
-
+	
 	fOut.Write(salt)
 	fOut.Write(masterNonce)
-
+	
 	key := deriveKey(password, salt)
 	block, _ := aes.NewCipher(key)
 	aesgcm, _ := cipher.NewGCM(block)
-
+	
 	buf := make([]byte, chunkSize)
 	fullNonce := make([]byte, 12)
 	copy(fullNonce[:8], masterNonce)
 	var counter uint32
-
+	
 	for {
 		n, err := fIn.Read(buf)
 		if n > 0 {
@@ -68,35 +85,44 @@ func encryptFile(src, dst, password string) error {
 	return nil
 }
 
-func decryptFile(src, dst, password string) error {
-	fIn, err := os.Open(src)
+func decryptFile(src, dst, password string, compatMode bool) error {
+	data, err := os.ReadFile(src)
 	if err != nil { return err }
-	defer fIn.Close()
 
+	if compatMode {
+		if len(data) < 12 { return fmt.Errorf("invalid file size") }
+		key := deriveKeyWeb(password)
+		block, _ := aes.NewCipher(key)
+		aesgcm, _ := cipher.NewGCM(block)
+		
+		iv, ciphertext := data[:12], data[12:]
+		plain, err := aesgcm.Open(nil, iv, ciphertext, nil)
+		if err != nil { return err }
+		return os.WriteFile(dst, plain, 0644)
+	}
+
+	fIn, _ := os.Open(src); defer fIn.Close()
 	salt := make([]byte, saltSize)
 	masterNonce := make([]byte, noncePrefixSize)
-	if _, err := io.ReadFull(fIn, salt); err != nil { return err }
-	if _, err := io.ReadFull(fIn, masterNonce); err != nil { return err }
-
-	fOut, err := os.Create(dst)
-	if err != nil { return err }
-	defer fOut.Close()
-
+	io.ReadFull(fIn, salt)
+	io.ReadFull(fIn, masterNonce)
+	
+	fOut, _ := os.Create(dst); defer fOut.Close()
 	key := deriveKey(password, salt)
 	block, _ := aes.NewCipher(key)
 	aesgcm, _ := cipher.NewGCM(block)
-
+	
 	fullNonce := make([]byte, 12)
 	copy(fullNonce[:8], masterNonce)
 	var counter uint32
-
+	
 	buf := make([]byte, chunkSize+tagSize)
 	for {
 		n, err := fIn.Read(buf)
 		if n > 0 {
 			binary.BigEndian.PutUint32(fullNonce[8:], counter)
 			plain, err := aesgcm.Open(nil, fullNonce, buf[:n], nil)
-			if err != nil { return fmt.Errorf("integrity check failed at chunk %d", counter) }
+			if err != nil { return fmt.Errorf("integrity check failed") }
 			fOut.Write(plain)
 			counter++
 		}
@@ -110,6 +136,7 @@ func main() {
 	pass := flag.String("p", "", "password")
 	input := flag.String("i", "", "input file/folder")
 	output := flag.String("o", "", "output directory")
+	compat := flag.Bool("compat", false, "compatibility mode for encryptor.html")
 	flag.Parse()
 
 	if *pass == "" || *input == "" || *output == "" {
@@ -124,9 +151,9 @@ func main() {
 		os.MkdirAll(filepath.Dir(outPath), 0755)
 
 		if *mode == "enc" {
-			return encryptFile(path, outPath+".enc", *pass)
+			return encryptFile(path, outPath+".enc", *pass, *compat)
 		} else {
-			return decryptFile(path, strings.TrimSuffix(outPath, ".enc"), *pass)
+			return decryptFile(path, strings.TrimSuffix(outPath, ".enc"), *pass, *compat)
 		}
 	})
 }
